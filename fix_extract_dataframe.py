8ece3b99265e6e937a3205c22134d6a5413ac55a
Selam <84750917+sel6@users.noreply.github.com>
2022-04-25 11:55:24 -0800

    statuses_count

--- fix_extract_dataframe.py
+++ fix_extract_dataframe.py
@@ -35,7 +35,8 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = []
+        
         
     def find_full_text(self)->list:
         text = 

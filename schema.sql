43dbab33c3a68de7b11b38595cb99848c36a459f
Selam <84750917+sel6@users.noreply.github.com>
2022-04-29 08:56:26 -0800

    add drop

--- schema.sql
+++ schema.sql
@@ -1,19 +1,23 @@
-DROP TABLE IF EXISTS tweet_data;
-
-CREATE TABLE IF NOT EXISTS tweet_data(
-id int not null AUTO_INCREMENT,
-created_at TEXT NOT NULL,
-source VARCHAR(200) NOT NULL,
-original_text TEXT DEFAULT NULL,
-polarity FLOAT DEFAULT NULL,
-subjectivity FLOAT DEFAULT NULL,
-lang TEXT DEFAULT NULL,
-favorite_count INT DEFAULT NULL,
-original_author TEXT DEFAULT NULL,
-followers_count INT DEFAULT NULL,
-friends_count INT DEFAULT NULL,
-possibly_sensitive TEXT DEFAULT NULL,
-hashtags TEXT DEFAULT NULL,
-user_mentions TEXT DEFAULT NULL,
-place TEXT DEFAULT NULL,
-place_coordinate VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id));
+DROP TABLE IF EXISTS `TweetInformation`;
+CREATE TABLE IF NOT EXISTS `TweetInformation` 
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `created_at` TEXT NOT NULL,
+    `source` VARCHAR(200) NOT NULL,
+    `clean_text` TEXT DEFAULT NULL,
+    `polarity` FLOAT DEFAULT NULL,
+    `subjectivity` FLOAT DEFAULT NULL,
+    `language` TEXT DEFAULT NULL,
+    `favorite_count` INT DEFAULT NULL,
+    `retweet_count` INT DEFAULT NULL,
+    `original_author` TEXT DEFAULT NULL,
+    `screen_count` INT NOT NULL,
+    `followers_count` INT DEFAULT NULL,
+    `friends_count` INT DEFAULT NULL,
+    `hashtags` TEXT DEFAULT NULL,
+    `user_mentions` TEXT DEFAULT NULL,
+    `place` TEXT DEFAULT NULL,
+    `place_coordinate` VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;

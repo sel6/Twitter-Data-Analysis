6bcc448e386a688dbf4a6670d979fe3b2c0806d6
Selam <84750917+sel6@users.noreply.github.com>
2022-05-08 09:23:19 -0800

    Update add_data.py

--- add_data.py
+++ add_data.py
@@ -1,134 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 12:02:25 2022
+
+@author: Selu
+"""
+
+# Importing module
 import os
 import pandas as pd
-import mysql.connector as mysql
 from mysql.connector import Error
+import mysql.connector as cu
 
 def DBConnect(dbName=None):
-  
-    conn = mysql.connect(host='localhost', user='root', password="Selam@0102.",
+    mydb = cu.connect(host='localhost', user='root',
+                      password='Selam@0102.',
                          database=dbName, buffered=True)
-    cur = conn.cursor()
-    return conn, cur
+    cursor = mydb.cursor()
+    return mydb, cursor
 
 
 def createDB(dbName: str) -> None:
-    """
-    Parameters
-    ----------
-    dbName :
-        str:
-    dbName :
-        str:
-    dbName:str :
-    Returns
-    -------
-    """
-    conn, cur = DBConnect()
-    cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
-    conn.commit()
-    cur.close()
-
+    mydb, cursor = DBConnect()
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
+    mydb.commit()
+    cursor.close()
+    
 def createTables(dbName: str) -> None:
-
-    conn, cur = DBConnect(dbName)
+    mydb, cursor = DBConnect(dbName)
     sqlFile = 'schema.sql'
     fd = open(sqlFile, 'r')
-    readSqlFile = fd.read()
+    readsqlFile = fd.read()
     fd.close()
-
-    sqlCommands = readSqlFile.split(';')
-
+    sqlCommands = readsqlFile.split(';')
     for command in sqlCommands:
         try:
-            res = cur.execute(command)
-        except Exception as ex:
-            print("Command skipped: ", command)
-            print(ex)
-    conn.commit()
-    cur.close()
-
-    return
-
+            result = cursor.execute(command)
+        except Exception as e:
+            print('command skipped: ', command)
+            print(e)
+    mydb.commit()
+    cursor.close()
+    
 def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Parameters
-    ----------
-    df :
-        pd.DataFrame:
-    df :
-        pd.DataFrame:
-    df:pd.DataFrame :
-    Returns
-    -------
-    """
-    cols_2_drop = ['Unnamed: 0', 'timestamp', 'sentiment', 'possibly_sensitive', 'cleaned_text']
     try:
-        df = df.drop(columns=cols_2_drop, axis=1)
         df = df.fillna(0)
     except KeyError as e:
-        print("Error:", e)
-
+        print('Error: ', e)
+    
     return df
-
-
-def insert_tweet(dbName: str, df: pd.DataFrame, table_name: str) -> None:
-   
-    conn, cur = DBConnect(dbName)
-
+        
+def insert_into_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> None:
+    mydb, cursor = DBConnect(dbName)
     df = preprocess_df(df)
-
     for _, row in df.iterrows():
-        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, cleaned_text, polarity, subjectivity, lang,
+        sqlQuery = f"""INSERT INTO {table_name} 
+        (created_at, source, original_text, polarity, subjectivity, lang,
                     favorite_count, retweet_count, original_author, followers_count, friends_count,
-                    hashtags, user_mentions, place)
-             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+                    hashtags, place)
+              VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+       
         data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
-                row[12], row[13])
-
+          row[12])
         try:
-            # Execute the SQL command
-            cur.execute(sqlQuery, data)
-            # Commit your changes in the database
-            conn.commit()
-            print("Inserted the tweets")
+            cursor.execute(sqlQuery, data)
+            mydb.commit()
+            print('Data inserted successfully')
         except Exception as e:
-            conn.rollback()
-            print("Error: ", e)
-    return
-
-def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
-   
-    connection, cursor1 = DBConnect(**kwargs)
-    if many:
-        cursor1.executemany(*args)
-    else:
-        cursor1.execute(*args)
-
-    # get column names
-    field_names = [i[0] for i in cursor1.description]
-
-    # get column values
-    res = cursor1.fetchall()
-
-    # get row count and show info
-    nrow = cursor1.rowcount
-    if tablename:
-        print(f"{nrow} recrods fetched from {tablename} table")
-
-    cursor1.close()
-    connection.close()
-
-    # return result
-    if rdf:
-        return pd.DataFrame(res, columns=field_names)
-    else:
-        return res
-
-
-if __name__ == "__main__":
+            mydb.rollback()
+            print('Error: ', e)
+            
+def fetch_data(table_name):
+    mydb, cursor= DBConnect()
+    column = []
+    query = "SELECT * FROM {table_name}"
+    value = cursor.execute(query)
+    for items in cursor.description:
+        column.append(items[0])
+        mydb.commit()
+        df = pd.DataFrame(value, columns=column)
+        cursor.close()
+        return df
+            
+if __name__=="__main__":
     createDB(dbName='tweets')
+    df = pd.read_csv('processed_tweet_data.csv')
     createTables(dbName='tweets')
-
-    df = pd.read_csv('clean_processed_tweet_data.csv')
-
-    insert_tweet(dbName='tweets', df=df, table_name='TweetInformation')
+    insert_into_tweet_table(dbName = 'tweets', df = df, table_name='TweetInformation')
